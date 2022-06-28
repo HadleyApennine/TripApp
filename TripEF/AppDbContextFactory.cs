52ed66fff524464bbfd0e4a011a777c45877b7a8
Karina Królikowska <Karina.Krolikowska@microsoft.wsei.edu.pl>
2022-06-29 00:19:02 +0200

    Add AppDbContextFactory.cs

---
+++ TripEF/AppDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TripEF;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args = null)
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>();
+        options.UseSqlServer("Server=.; Database=TripDatabase; Trusted_Connection=True");
+        return new AppDbContext(options.Options);
+    }
+}
